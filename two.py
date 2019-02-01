1ab76bc231f7d8ff52959faa4686016cf06e25eb
Dukerider45 <46587258+Dukerider45@users.noreply.github.com>
2019-02-01 20:46:20 +0530

    Update two.py

--- two.py
+++ two.py
@@ -1,6 +1,12 @@
-n=int(input())
-count=0
-while(n>0):
-    count=count+1
-    n=n//10
-    print("")
+
+def main():
+    c = 0
+    n = int(input())
+    while (n > 0):
+      n = n//10
+      c = c + 1
+    print (c)
+
+if __name__ == '__main__':
+    main()
+
